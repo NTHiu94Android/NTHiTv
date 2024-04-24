83b0b0ab0c8a164d94dbe9d18d714f442c764c32
NguyenTrongHieu <98166250+NTHiu94Android@users.noreply.github.com>
2024-04-24 11:13:33 +0700

    update

--- src/screens/SRC002/Src00201.jsx
+++ src/screens/SRC002/Src00201.jsx
@@ -1,6 +1,6 @@
 import {
   Text, View, ScrollView, TouchableOpacity, Alert, Pressable,
-  TextInput, Platform, Image, PermissionsAndroid
+  TextInput, Platform, Image, PermissionsAndroid, StatusBar, ActivityIndicator
 } from 'react-native'
 import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
@@ -11,79 +11,101 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import CustomAxios from '../../helpers/FetchApi';
 import Loading from '../../components/TTLoading';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import VideoView from 'react-native-easy-video'
 
-//Xac minh tai lieu
-const Src00201 = ({route}) => {
+//Xem video
+const Src00201 = ({ route }) => {
   const navigation = useNavigation();
   const [movieId, setMovieId] = useState(null)
+  const [videoSource, setVideoSource] = useState('https://fetch.cloudz.win/tt/play/260510ea-f015-44e3-a2bd-aa4a65f6ff4c.m3u8');
+  const [lstVideo, setLstVideo] = useState([]);
+  const [videoSelected, setVideoSelected] = useState(null);
   useEffect(() => {
-    console.log('route', route);
-    if(route.params && route.params.movie_id){
+    if (route.params && route.params.movie_id) {
       setMovieId(route.params.movie_id);
-      console.log('route.params.movie_id', route.params.movie_id);
+      fetchData(route.params.movie_id);
     }
   }, []);
 
   //------------FetchData--------------
-  // const fetchData = async (userPk) => {
-  //   const body = {
-  //     pro: 'EC_SYS_MBI_SEL_CUST001_001_1',
-  //     data: [userPk]
-  //   };
-  //   console.log('body: ', body);
-  //   CustomAxios().post('/api/exec-auth', body)
-  //     .then(res => {
-  //       if(!res) {
-  //         Alert.alert('Thông báo', 'Có lỗi xảy ra, vui lòng thử lại sau');
-  //       }
-  //       if (res.error) {
-  //         Alert.alert('Thông báo', res.message);
-  //       }
-  //       if (res.message == 'Token expired') {
-  //         RefreshToken({
-  //           token: REFRESH_TOKEN,
-  //           username: userPk,
-  //           calback: fetchData(userPk),
-  //           logout: async () => {
-  //             await AsyncStorage.clear();
-  //             navigation.reset({ index: 0, routes: [{ name: "SRC00001" }] });
-  //           }
-  //         });
-  //         return;
-  //       }
-  //       if (res.data){
-
-  //       }
-  //     })
-  //     .catch(err => {
-  //       console.log('Error: ', err);
-  //     });
-  // };
+  const fetchData = async (movieId) => {
+    const body = {
+      pro: 'NTH_MV_SEL_LEP_001',
+      data: [movieId]
+    };
+    console.log('body: ', body);
+    CustomAxios().post('/api/exec-no-auth', body)
+      .then(res => {
+        if (!res) {
+          Alert.alert('Thông báo', 'Có lỗi xảy ra, vui lòng thử lại sau');
+        }
+        if (res.error) {
+          Alert.alert('Thông báo', res.message);
+        }
+        // if (res.message == 'Token expired') {
+        //   RefreshToken({
+        //     token: REFRESH_TOKEN,
+        //     username: userPk,
+        //     calback: fetchData(userPk),
+        //     logout: async () => {
+        //       await AsyncStorage.clear();
+        //       navigation.reset({ index: 0, routes: [{ name: "SRC00001" }] });
+        //     }
+        //   });
+        //   return;
+        // }
+        if (res.data && res.data[0]) {
+          console.log('res.data: ', res.data[0]);
+          setLstVideo(res.data[0]);
+          if(res.data[0][0]){
+            setVideoSelected(res.data[0][0]);
+            setVideoSource(res.data[0][0].link);
+          }
+        }
+      })
+      .catch(err => {
+        console.log('Error: ', err);
+      });
+  };
 
 
   return (
     <View style={{ backgroundColor: Color.backgroundColor, flex: 1 }}>
-      <TTHeader
-        width='100%'
-        hasTitle={true}
-        title='Xác thực thông tin cửa hàng'
-        hasIconRight1={true}
-        iconRight1='headset'
-        iconRightOnPress={() => {
-          console.log('headset');
-        }}
+      <StatusBar
+        translucent={true}
+        backgroundColor={"transparent"}
+        barStyle="light-content"
       />
+      <View style={{ paddingTop: 30, }}>
+        {videoSource ? (
+          <VideoView
+            source={{ uri: videoSource, type: 'm3u8' }}
+            height={250}
+            controls={true}
+            resizeMode="contain"
+            allowsExternalPlayback={true}
+            autoPlay={true}
+            selectedTextTrack={{ type: 'title', value: 'English' }}
+            goBack={() => navigation.goBack()}
+          />
+        ) : (
+          <View style={{
+            height: 250, width: '100%',
+            backgroundColor: '#ddd',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <ActivityIndicator size="small" color="#fff" />
+            <Text style={{ color: '#fff', marginTop: 10 }}>
+              Đang tải video...
+            </Text>
+          </View>
+        )}
+      </View>
+      <ScrollView style={{ flex: 1, padding: 10, minHeight: 300, backgroundColor: 'yellow' }}>
 
-      <KeyboardAwareScrollView
-        style={{ flex: 1 }}
-        contentContainerStyle={{ flexGrow: 1, }}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      >
-        <ScrollView style={{ flex: 1, padding: 10 }}>
-          
 
-        </ScrollView>
-      </KeyboardAwareScrollView>
+      </ScrollView>
 
     </View>
   )
